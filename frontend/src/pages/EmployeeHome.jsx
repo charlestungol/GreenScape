41971aa95c816aa012bd0133d03d0b2cb07ba4c5
aileenpearl <aileenpearl.quinonez@edu.sait.ca>
2026-02-11 20:33:01 -0700

    updating the employee homepage

--- frontend/src/pages/EmployeeHome.jsx
+++ frontend/src/pages/EmployeeHome.jsx
@@ -1,9 +1,8 @@
-const EmployeeHome = () =>{
-    return(
-        <div>
-            <p className="titleWrapper">EMPLOYEE DASHBOARD</p>
-        </div>
-    )
-}
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const EmployeeHome = () => {
+  return <Navigate to="/employee/dashboard" replace />;
+};
 
 export default EmployeeHome;
