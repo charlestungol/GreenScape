6ad676aa1dbb6a1469b104dab82641cafab25ef7
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2026-03-05 12:25:32 -0700

    Fix bug on settings

--- backend/auth/settings.py
+++ backend/auth/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 # Email verifty
 SITE_ID = 1
-ACCOUNT_LOGIN_METHODS = {"email"}
+ACCOUNT_LOGIN_METHODS = ["email"]
 ACCOUNT_SIGNUP_FIELDS = ["email*", "password1*", "password2*"]
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
